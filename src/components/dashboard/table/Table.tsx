7f91503139723a3d0e11c255206286f9d19d43d2
victor <oguntayovictor65@gmail.com>
2025-01-18 08:19:44 +0100

    implment table heading ui

--- src/components/dashboard/table/Table.tsx
+++ src/components/dashboard/table/Table.tsx
@@ -4,12 +4,12 @@ const Table = () => {
   return (
     <table className="table">
       <thead>
-        <tr>
+        <tr className="table__header">
           <TableTitle name="organization" />
           <TableTitle name="username" />
           <TableTitle name="email" />
           <TableTitle name="phone number" />
-          <TableTitle name="dat joined" />
+          <TableTitle name="date joined" />
           <TableTitle name="status" />
         </tr>
       </thead>
